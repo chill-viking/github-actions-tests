9177a6edc062cb97ada635be685b6cacbb77f8c9
Peter Jokumsen <pjokumsen@gmail.com>
2024-08-26 19:42:13 +0200

    feat!: target `net8.0` and upgrade dependencies
    - Updated projects to target `net8.0`
    - Migrated to `NSubstitute` from `Moq`
    - Upgraded all dependencies

--- dotnet-sonar-scanner/GitHubAction.Console.Tests/CalculatorTests.cs
+++ dotnet-sonar-scanner/GitHubAction.Console.Tests/CalculatorTests.cs
@@ -1,45 +1,47 @@
 using GitHubAction.ClassLibrary;
-using Moq;
+using NSubstitute;
 
 namespace GitHubAction.Console.Tests;
 
 public class CalculatorTests
 {
-    private readonly Mock<IMathsHelper> _goodMathsMock = new();
-    private readonly Mock<IMathsHelper> _badMathsMock = new();
+    private IMathsHelper _goodMaths;
+    private IMathsHelper _badMaths;
 
     private Calculator _calculator = null!;
 
     [SetUp]
     public void BeforeEach()
     {
-        _badMathsMock.Reset();
-        _goodMathsMock.Reset();
+        _badMaths = Substitute.For<IMathsHelper>();
+        _goodMaths = Substitute.For<IMathsHelper>();
 
-        _calculator = new Calculator(_goodMathsMock.Object, _badMathsMock.Object);
+        _calculator = new Calculator(_goodMaths, _badMaths);
     }
 
     [Test]
     public void GetSum_CorrectIsTrue_UsesGoodMaths()
     {
-        _goodMathsMock.Setup(m => m.Sum(It.IsAny<int>(), It.IsAny<int>()))
+        _goodMaths
+            .Sum(Arg.Any<int>(), Arg.Any<int>())
             .Returns(15);
 
         var actual = _calculator.GetSum(1, 2);
 
-        _goodMathsMock.Verify(m => m.Sum(1, 2));
+        _goodMaths.Received().Sum(1, 2);
         Assert.That(actual, Is.EqualTo(15));
     }
 
     [Test]
     public void GetSum_CorrectIsFalse_UsesBadMaths()
     {
-        _badMathsMock.Setup(m => m.Sum(It.IsAny<int>(), It.IsAny<int>()))
+        _badMaths
+            .Sum(Arg.Any<int>(), Arg.Any<int>())
             .Returns(123);
 
         var actual = _calculator.GetSum(1, 2, false);
 
-        _badMathsMock.Verify(m => m.Sum(1, 2));
+        _badMaths.Received().Sum(1, 2);
         Assert.That(actual, Is.EqualTo(123));
     }
 }
